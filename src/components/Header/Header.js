c327d810c191a8bad6da51ac73fa69b031fa5e86
0178811jes <jesminara8653@gmail.com>
2022-09-28 22:17:57 +0600

    first commit title

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+            <h1>SPORTS-ACTIVE-CLUB</h1>
+        </div>
+    );
+};
+
+export default Header;
