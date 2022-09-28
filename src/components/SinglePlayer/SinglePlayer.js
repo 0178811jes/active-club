3d3182d108e9aa0837a75075ca358c862129f6f4
0178811jes <jesminara8653@gmail.com>
2022-09-29 03:40:05 +0600

    first commit title

--- src/components/SinglePlayer/SinglePlayer.js
+++ src/components/SinglePlayer/SinglePlayer.js
@@ -7,13 +7,13 @@ const SinglePlayer = (props) => {
     return (
         <div className='player'>
             <img src={strSportThumb} alt="" />
-            {/* <h4>Sports Name:{strSport}</h4>
+            <h4>Sports Name:{strSport}</h4>
             <p>Id:{idSport}</p>
             <p><small>Team Name:{strFormat}</small></p>
             
             <button className='btn'>
                 <p>Add To List</p>
-            </button> */}
+            </button>
         </div>
     );
 };
