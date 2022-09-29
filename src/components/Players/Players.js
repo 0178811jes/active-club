73036dc01e85ebd445bb2bf03c81e8c5c690c928
0178811jes <jesminara8653@gmail.com>
2022-09-29 08:06:00 +0600

    first commit title

--- src/components/Players/Players.js
+++ src/components/Players/Players.js
@@ -6,6 +6,7 @@ import './Players.css';
 
 const Players = () => {
     const [players , setPlayers] = useState([]);
+    const [count, setCount] = useState([]);
 
     useEffect(()=>{
         fetch('players.json')
@@ -14,7 +15,13 @@ const Players = () => {
         
         .then(data=> setPlayers(data.sports));
         
-    } , [])
+    } , []);
+
+    const handlerAddToList =(player)=>{
+        console.log(player);
+        const newCaunt =[...count,player];
+        setCount(newCaunt);
+    }
 
 
     return (
@@ -22,13 +29,35 @@ const Players = () => {
             <div className="sports-container">
                {
                 players.map(player=> <SinglePlayer 
-                   
+                handlerAddToList={handlerAddToList}
                 player={player}
                 ></SinglePlayer>)
                }
             </div>
             <div className="cart-container">
-                <h2>My name is .....</h2>
+                <div>
+                <h4>Jesmin Ara</h4>
+                <p>I lives in Dhaka</p>
+                <p>My home district in Rajshahi</p>
+                </div>
+                <div>
+                    <h4>Add A Break</h4>
+                    <div className='add-break'>
+                        <button>10m</button>
+                        <button>5m</button>
+                        <button>15m</button>
+                        <button>20m</button>
+                        
+                    </div>
+                    <div>
+                        <h2>Exercise Details</h2>
+                        <div>
+                            <h4>Exercise Time:{count.length}</h4>
+                           
+                            <h4>Break Time:</h4>
+                        </div>
+                    </div>
+                </div>
             </div>
 
         </div>
